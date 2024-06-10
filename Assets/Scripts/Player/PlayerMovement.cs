b8bce3314c0396f698fd56b8019e1480502458e7
Pencil-Push <KnX72aL@gmail.com>
2024-06-10 18:47:44 -0400

    Movement+Anim+Idle

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,15 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header ("Player Components")]
     private Rigidbody2D rb;
-    //private Animator dAnim;
+    private Animator dAnim;
 
     [Header ("Move Components")]
     [SerializeField] private float moveSpeed;
     private Vector2 _movement;
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        dAnim = GetComponent<Animator>();
     }
 
     void Update()
@@ -22,5 +27,14 @@ public class PlayerMovement : MonoBehaviour
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
 
         rb.velocity = _movement * moveSpeed;
+
+        dAnim.SetFloat(_horizontal, _movement.x);
+        dAnim.SetFloat(_vertical, _movement.y);
+
+        if(_movement != Vector2.zero)
+        {
+            dAnim.SetFloat(_lastHorizontal, _movement.x);
+            dAnim.SetFloat(_lastVertical, _movement.y);
+        }
     }
 }
